2942afde17b469ac2ac3554f7912cecaac376967
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-08-11 15:31:40 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,10 @@ fileNum = $(find ./branchA -type f)
 
 if [[ $fileNum == 5 ]]
 then
+  echo "Loading Script"
+else
   echo "Please create 5 files in branchA"
+  exit 1
 fi
 
 if [[ -d ./branchA ]]
@@ -14,4 +17,5 @@ then
  touch ./CorpFolder/DevTeam/file_{A..Z} && touch ./CorpFolder/DataTeam/file_{A..Z} && touch ./CorpFolder/OpTea/file_{A..Z}
 else 
   echo "Please run the script in the Desktop Directory and you've already created 5 files in the branchA directory"
+  exit 1
 fi
