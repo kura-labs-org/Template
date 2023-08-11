8f2c0276081faec35e2978b5252caf0514647184
kura-labs01 <86966558+kura-labs01@users.noreply.github.com>
2023-08-11 15:57:27 -0400

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-fileNum = $(find ./branchA -type f 2> /dev/null)
+fileNum=$(find ./branchA -type f | wc -l)
 
-if [[ $fileNum == 5 2> /dev/null ]]
+if [[ $fileNum == 5 ]]
 then
   echo "Loading Script"
 else
@@ -13,9 +13,9 @@ fi
 if [[ -d ./branchA ]]
 then
  echo "Creating CorpFolder"
- mkdir ./CorpFolder/DevTeam && mkdir ./CorpFolder/DataTeam && mkdir ./CorpFolder/OpTeam
- touch ./CorpFolder/DevTeam/file_{A..Z} && touch ./CorpFolder/DataTeam/file_{A..Z} && touch ./CorpFolder/OpTea/file_{A..Z}
-else 
+ mkdir ./CorpFolder && mkdir ./CorpFolder/DevTeam && mkdir ./CorpFolder/DataTeam && mkdir ./CorpFolder/OpTeam
+ touch ./CorpFolder/DevTeam/file_{A..Z} && touch ./CorpFolder/DataTeam/file_{A..Z} && touch ./CorpFolder/OpTeam/file_{A..Z}
+else
   echo "Please run the script in the Desktop Directory and you've already created 5 files in the branchA directory"
   exit 1
 fi
